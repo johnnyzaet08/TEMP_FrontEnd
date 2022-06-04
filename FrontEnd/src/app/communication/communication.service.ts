d4e6aa213c237bb1b279b149bacd8da2ec77f7c4
johnnyzaet08 <johnny.zaet08@gmail.com>
2022-06-04 16:10:39 -0600

    Cambios basicos
    Hello

--- FrontEnd/src/app/communication/communication.service.ts
+++ FrontEnd/src/app/communication/communication.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class CommunicationService {
 
-  private URL = "AQUI VA LA URL";
+  private URL = "https://apistraviatec.azurewebsites.net/";
 
   constructor(private http: HttpClient) {} 
    // LOGIN - INICIO DE SESIÓN | VERIFICACIÓN DE USUARIO ATLETA
@@ -83,15 +83,15 @@ export class CommunicationService {
 
   //SEND REGISTER DATA
   public sendRegisterDataAtl(fname, lname, nationality, bDate, age, user, pass, url, category){
-   return this.http.post<JSON>(this.URL+"api/athlete/Register",{
-     "fName":fname,
-     "lName":lname,
+   return this.http.post<JSON>(this.URL+"api/Athlete",{
+     "name":fname,
+     "lname1":lname,
      "nationality":nationality,
-     "bDate":bDate,
-     "age":age,
+     "birth_date":bDate,
+     "current_age":age,
      "username":user,
      "password":pass,
-     "img_url":url,
+     "photo":url,
      "category":category
       });
    }
